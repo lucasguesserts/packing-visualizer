e2d601299c248c37135f4515d6c427542998e4c5
Lucas Guesser Targino da Silva <lucasguesserts@gmail.com>
2023-04-28 22:39:45 -0300

    feat: log information about all overlaps between items

--- src/check/OutputChecker.js
+++ src/check/OutputChecker.js
@@ -10,8 +10,13 @@ class OutputChecker {
       default:
         console.error(`The version ${data.version} or the output is not supported`)
     }
-    if (OutputChecker._hasOverlap(data)) {
-      console.error('overlap found')
+    const overlaps = OutputChecker._findOverlaps(data)
+    if (overlaps.length) {
+      for (const overlap of overlaps) {
+        console.error(`There is an overlap between the items ${overlap.firstIndex} and ${overlap.secondIndex}`)
+        console.error(`first item:\n${JSON.stringify(overlap.first)}`)
+        console.error(`second item:\n${JSON.stringify(overlap.second)}`)
+      }
     }
   }
 
@@ -26,19 +31,23 @@ class OutputChecker {
     }
   }
 
-  static _hasOverlap (data) {
+  static _findOverlaps (data) {
     const TOLERANCE = 1e-6
     const items = data.small_items
-    for (const first of items) {
-      for (const second of items) {
-        if (first === second) continue
+    const overlaps = []
+    for (let firstIndex = 0; firstIndex < items.length; ++firstIndex) {
+      const first = items[firstIndex]
+      for (let secondIndex = firstIndex + 1; secondIndex < items.length; ++secondIndex) {
+        const second = items[secondIndex]
         const overlapX = ((first.x + TOLERANCE) < (second.x + second.length)) && ((second.x + TOLERANCE) < (first.x + first.length))
         const overlapY = ((first.y + TOLERANCE) < (second.y + second.length)) && ((second.y + TOLERANCE) < (first.y + first.length))
         const overlapZ = ((first.z + TOLERANCE) < (second.z + second.length)) && ((second.z + TOLERANCE) < (first.z + first.length))
-        if (overlapX && overlapY && overlapZ) return true
+        if (overlapX && overlapY && overlapZ) {
+          overlaps.push({ firstIndex, first, secondIndex, second })
+        }
       }
     }
-    return false
+    return overlaps
   }
 }
 
