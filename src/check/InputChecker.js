83302bf691a08cdd68fa82663917f4cfe9b4dd91
Lucas Guesser Targino da Silva <lucasguesserts@gmail.com>
2023-04-30 00:18:37 -0300

    refactor: create class 'FileFormatChecker' for the input

--- src/check/InputChecker.js
+++ src/check/InputChecker.js
@@ -1,26 +1,9 @@
-import Ajv from 'ajv/dist/2020'
-import v_0_0_0 from '../file_format/input/0_0_0.json' // eslint-disable-line camelcase
+import FileFormatChecker from './input/FileFormatChecker.js'
 
 class InputChecker {
   static check (data) {
-    switch (data.version) {
-      case '0.0.0':
-        InputChecker._check_v_0_0_0(data)
-        break
-      default:
-        console.error(`The version ${data.version} or the input is not supported`)
-    }
-  }
-
-  static _check_v_0_0_0 (data) {
-    const ajv = new Ajv()
-    const validate = ajv.compile(v_0_0_0)
-    const isValid = validate(data)
-    if (!isValid) {
-      console.error(validate.errors)
-    } else {
-      console.log('No problem found in input file')
-    }
+    const fileFormatChecker = new FileFormatChecker(data)
+    fileFormatChecker.log(data)
   }
 }
 
---
+++ src/check/input/FileFormatChecker.js
@@ -0,0 +1,39 @@
+import Ajv from 'ajv/dist/2020'
+import v_0_0_0 from '../../file_format/input/0_0_0.json' // eslint-disable-line camelcase
+
+class FileFormatChecker {
+  constructor (data) {
+    this.data = data
+    this.validator = this._getValidator(this.data.version)
+    this.isValid = this.validator(data)
+    this.errors = this.validator.errors
+  }
+
+  log () {
+    if (!this.isValid) {
+      console.error(this.errors)
+    } else {
+      console.log('No problem found in input file')
+    }
+  }
+
+  _getValidator (version) {
+    const ajv = new Ajv()
+    switch (version) {
+      case '0.0.0':
+        return ajv.compile(v_0_0_0)
+      default:
+        return this._invalidVersionValidator()
+    }
+  }
+
+  _invalidVersionValidator () {
+    function Validator (data) {
+      return false
+    }
+    Validator.errors = `The version ${this.data.version} of the input is not supported`
+    return Validator
+  }
+}
+
+export default FileFormatChecker
