e5a830748184ec55432de3c663ad13affb01951f
Lucas Guesser Targino da Silva <lucasguesserts@gmail.com>
2023-04-30 00:19:00 -0300

    test: add test to 'input/FileFormatChecker'

---
+++ test/check/input/FileFormatChecker.test.js
@@ -0,0 +1,25 @@
+import FileFormatChecker from '../../../src/check/input/FileFormatChecker.js'
+import fs from 'fs'
+import path from 'path'
+
+const dataDir = './test/data/check/input/FileFormatChecker/'
+
+describe('outside items finder', () => {
+  const cases = [
+    ['1', true],
+    ['2', true],
+    ['3', false],
+    ['4', false],
+    ['5', false],
+    ['6', false],
+    ['7', false]
+  ]
+  for (const [caseName, expected] of cases) {
+    test(`case ${caseName}`, () => {
+      const filePath = path.join(dataDir, `${caseName}.json`)
+      const data = JSON.parse(fs.readFileSync(filePath))
+      const checker = new FileFormatChecker(data)
+      expect(checker.isValid).toEqual(expected)
+    })
+  }
+})
