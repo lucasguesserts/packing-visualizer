19605ceb354993b7ece1ee7fa753640e1d03a21b
Lucas Guesser Targino da Silva <lucasguesserts@gmail.com>
2023-06-08 18:45:47 -0300

    test: add test to input file format check version 0.1.0

--- test/check/input/FileFormatChecker.test.mjs
+++ test/check/input/FileFormatChecker.test.mjs
@@ -24,3 +24,24 @@ describe('0.0.0', () => {
     })
   }
 })
+
+describe('0.1.0', () => {
+  const subdirName = '0_1_0/'
+  const cases = [
+    ['1', true],
+    ['2', true],
+    ['3', false],
+    ['4', false],
+    ['5', false],
+    ['6', false],
+    ['7', false]
+  ]
+  for (const [caseName, expected] of cases) {
+    test(`case ${caseName}`, () => {
+      const filePath = path.join(dataDir, subdirName, `${caseName}.json`)
+      const data = JSON.parse(fs.readFileSync(filePath))
+      const checker = new FileFormatChecker(data)
+      expect(checker.isValid).toEqual(expected)
+    })
+  }
+})
