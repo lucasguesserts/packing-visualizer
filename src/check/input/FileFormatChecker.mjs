0178b37924beef27a8c6749e64f9f574e084377e
Lucas Guesser Targino da Silva <lucasguesserts@gmail.com>
2023-06-08 18:45:26 -0300

    feat: add checker for input file format version 0.1.0

--- src/check/input/FileFormatChecker.mjs
+++ src/check/input/FileFormatChecker.mjs
@@ -1,5 +1,6 @@
 import Ajv from 'ajv/dist/2020'
 import v_0_0_0 from '../../file_format/input/0_0_0.json' // eslint-disable-line camelcase
+import v_0_1_0 from '../../file_format/input/0_1_0.json' // eslint-disable-line camelcase
 
 class FileFormatChecker {
   constructor (data) {
@@ -22,6 +23,8 @@ class FileFormatChecker {
     switch (version) {
       case '0.0.0':
         return ajv.compile(v_0_0_0)
+      case '0.1.0':
+        return ajv.compile(v_0_1_0)
       default:
         return this._invalidVersionValidator()
     }
