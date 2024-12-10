eafddeaab363707188d88ff4d41eb17c50cd0d4f
Lucas Guesser <github.process990@passinbox.com>
2024-12-10 17:30:43 -0300

    feat: make the colors of the Small Items deterministic

--- src/objects/SmallItemColors.mjs
+++ src/objects/SmallItemColors.mjs
@@ -2,14 +2,16 @@ import randomColor from 'randomcolor'
 
 class SmallItemColors {
   constructor (smallItems) {
+    const colors = randomColor({ seed: 42, count: smallItems.length, luminosity: 'dark' })
     this.color_map = new Map()
-    for (const item of smallItems) {
+    for (let i = 0; i < smallItems.length; ++i) {
+      const item = smallItems[i]
+      const color = colors[i]
       const hash = this.getHash(item)
       if (!this.color_map.has(hash)) {
-        this.color_map.set(hash, randomColor({ luminosity: 'dark' }))
+        this.color_map.set(hash, color)
       }
     }
-    return
   }
 
   getHash (item) {
