772bfeb282b5264448b730bc81aa2ee6acbbe3e9
Lucas Guesser <github.process990@passinbox.com>
2025-01-23 12:13:11 -0300

    feat: define color of item using its hash

--- src/objects/SmallItemColors.mjs
+++ src/objects/SmallItemColors.mjs
@@ -2,12 +2,11 @@ import randomColor from 'randomcolor'
 
 class SmallItemColors {
   constructor (smallItems) {
-    const colors = randomColor({ seed: 42, count: smallItems.length, luminosity: 'dark' })
     this.color_map = new Map()
     for (let i = 0; i < smallItems.length; ++i) {
       const item = smallItems[i]
-      const color = colors[i]
       const hash = this.getHash(item)
+      const color = randomColor({ seed: hash, luminosity: 'dark'})
       if (!this.color_map.has(hash)) {
         this.color_map.set(hash, color)
       }
